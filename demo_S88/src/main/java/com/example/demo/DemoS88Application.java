661e536af8d13684e6aebb4cd8e95b6d1a920765
Susana <sgonzalezg@correo.iue.edu.co>
2021-03-13 09:51:30 -0500

    13/03/2021 Ejemplo cambio
    Cambio en el codigo

--- demo_S88/src/main/java/com/example/demo/DemoS88Application.java
+++ demo_S88/src/main/java/com/example/demo/DemoS88Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoS88Application {
+	
+	//Cambio clase 13-03-2021
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoS88Application.class, args);
