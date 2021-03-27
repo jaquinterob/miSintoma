61a660e59b3420fc14c238e8fb886e7f20cf583f
Juan Gabriel <Perezjuang@hotmail.com>
2021-03-27 10:34:02 -0500

    27032021-Actulizar proyecto

---
+++ demoJGP/src/test/java/com/example/demo/DemoJgpApplicationTests.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(classes = DemoJgpApplication.class, 
+webEnvironment = WebEnvironment.RANDOM_PORT)
+class DemoJgpApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	
+	@Test
+	public void testAllEmployees() 
+	{
+	//  (Escenario)	
+	//  (Given) some context
+	//	(When) some action is carried out
+	//	(Then) a particular set of observable consequences should obtain
+	
+	/***	Un ejemplo:
+	        Retiro de un Cajero
+			Dado que mi cuenta bancaria tiene crédito y no hice retiros recientemente,
+			Cuando intento retirar una cantidad menor que el límite de mi tarjeta,
+			Entonces el retiro debe completarse sin errores ni advertencias.			
+			// JBehave, RSpec o Cucumber	https://openwebinars.net/blog/que-es-gherkin/		 
+	***/	
+		
+	//Escenario 1: Realizo el llamado a la URL me saluda con mi nombre 
+	 //GIVEN: Dado que el servicio puede ser llamado desde una URL enviando mi  nombre
+	 String URL = "http://localhost:" + port + "/greeting?name=Juan";
+	 //WHEN: Cuando invoco la URL 	
+	 String restRequest = this.restTemplate.getForObject(URL, Greeting.class).getContent();
+	 //THEN: El servicio responde el Saludo con mi nombre: Hello, Juan! 
+     assertTrue(restRequest.equals("Hello, Juan!XXXX"));
+     
+     
+	}
+
+}
