1687247410703ceaa608e172b71dd42d96cf6221
LuisQuimbayo <luismarin176@gmail.com>
2021-03-27 10:01:05 -0500

    Actualizando demo

--- demo_LQM/src/test/java/com/example/demo/DemoLaqmApplicationTests.java
+++ demo_LQM/src/test/java/com/example/demo/DemoLaqmApplicationTests.java
@@ -13,10 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DemoJaqbApplication.class, 
+@SpringBootTest(classes = DemoLaqmApplication.class, 
 webEnvironment = WebEnvironment.RANDOM_PORT)
 
-class DemoJaqbApplicationTests {
+class DemoLaqmApplicationTests {
 
 	@LocalServerPort
 	private int port;
