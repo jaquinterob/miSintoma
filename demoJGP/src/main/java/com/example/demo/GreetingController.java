cb17b2ba75d26ff21aca5b1a90dae6ce09e2745e
Juan Gabriel <Perezjuang@hotmail.com>
2021-04-16 17:39:46 -0500

    Update

--- demoJGP/src/main/java/com/example/demo/GreetingController.java
+++ demoJGP/src/main/java/com/example/demo/GreetingController.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
