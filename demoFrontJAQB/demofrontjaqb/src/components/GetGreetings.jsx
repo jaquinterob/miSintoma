c4d7dcd55ed9913b9bccda924262df43fb311ee6
John Alexander Quintero Berrío <john.quintero@coopsana.com.co>
2021-04-25 23:18:14 -0500

    Se adecua los nombres a pacientes y paciente

--- demoFrontJAQB/demofrontjaqb/src/components/GetGreetings.jsx
+++ demoFrontJAQB/demofrontjaqb/src/components/GetGreetings.jsx
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const GetGreeting = () => {
+const GetPaciente = () => {
     const apiUrl = "http://localhost:8282/"
 
-    const GreetingData = [
+    const PacienteData = [
         { id: 1, content: "Sin contenido" },
     ];
-    const [greeting, setGreeting] = useState(GreetingData);
+    const [pacientes, setPacientes] = useState(PacienteData);
     //const [showLoading, setShowLoading] = useState(true);
 
     useEffect(() => {
         const fechaData = async () => {
             const result = await axios(apiUrl + 'api/pacientesCustomAPI/getAllPacientes');
-            setGreeting(result.data);
+            setPacientes(result.data);
         };
         fechaData();
     }, []);
 
     return (
         <div style={{ color: '#031249' }}>
-        <h1>Peticion con Axios</h1>
+        <h1>Peticion de pacientes con Axios</h1>
             {
-                greeting.map(item => <p >id = {item.id} | nombre = {item.nombre} | sexo = {item.sexo} | edad = {item.edad} </p>)
+                pacientes.map(paciente => <p >id = {paciente.id} | nombre = {paciente.nombre} | sexo = {paciente.sexo} | edad = {paciente.edad} </p>)
             }
         </div>
     );
 
 };
 
-export default GetGreeting;
+export default GetPaciente;
