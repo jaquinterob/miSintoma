c83555b5571fdf07842aab84f8bd5f0a733272c3
M3nrva <antoniaestrada9@hotmail.com>
2021-06-11 18:17:07 -0500

    cambio

--- miSintomaFront/src/containers/App/App.jsx
+++ miSintomaFront/src/containers/App/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import './App.scss';
 
-
-const App = () => <div className="App">HOLA MUNDO</div>;
+const App = () => (
+    <div className="App">
+        HOLA MUNDO
+    </div>
+)
 export default App;
